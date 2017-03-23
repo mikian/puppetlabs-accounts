c1f14ff0ef35a4032cb12fa7d6fdc31bb55de908
Mikko Kokkonen <mkokkonen@stratajet.com>
2017-03-23 18:36:20 +0000

    Fixes

--- manifests/group.pp
+++ manifests/group.pp
@@ -1,18 +1,19 @@
 define accounts::group(
-  $ensure               = 'present',
-  $gid                  = undef,
+  $ensure = 'present',
+  $gid    = undef,
+  $system = false
 ) {
   validate_re($ensure, '^(present|absent)$')
+  validate_bool($system)
 
   if $gid != undef {
     validate_re($gid, '^\d+$')
-    $_gid = $gid
-  } else {
-    $_gid = $name
   }
+  $_gid = $gid
 
   group { $name:
     ensure => $ensure,
     gid    => $_gid,
+    system => $system
   }
 }
